d69e1007d80cf808231434384e77a21624cbcc89
alfredo <vdevendetta77@gmail.com>
2022-11-07 13:43:22 +0100

    Init h2 database with data

--- src/main/java/com/inditex/backend/Application.java
+++ src/main/java/com/inditex/backend/Application.java
@@ -1,4 +1,4 @@
-package com.hm.backend;
+package com.inditex.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/inditex/backend/ApplicationTests.java
+++ src/test/java/com/inditex/backend/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.hm.backend;
+package com.inditex.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
